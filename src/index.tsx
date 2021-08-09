a3e4ab4109ca3245ff959ee636f05f81949d3679
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-09 09:37:34 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import '@scss/index.scss'
 import React from 'react'
 import {render} from 'react-dom'
-import {App} from '@react/App'
+import {App} from './src/typescript/react/App'
 
 
 render(<App />, document.getElementById('root'))
