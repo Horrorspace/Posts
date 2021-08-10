75c66ba3df91b078081e04e1519364e59aa08fe2
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-10 12:38:02 +0700

    Create postActions.ts

---
+++ src/typescript/redux/actions/postActions.ts
@@ -0,0 +1,22 @@
+import {ActTypes} from '@redux/types/PostActTypes.ts'
+
+export const downloadPosts = (): IreduxAction => {
+    return {
+        type: PostActTypes.downloadPosts,
+    }
+};
+export const sendNewPost = (): IreduxAction => {
+    return {
+        type: PostActTypes.sendNewPost,
+    }
+};
+export const putPost = (): IreduxAction => {
+    return {
+        type: PostActTypes.putPost,
+    }
+};
+export const updatePosts = (): IreduxAction => {
+    return {
+        type: PostActTypes.updatePosts,
+    }
+};
