03c610901b7fcd63bc66c96670df2ca01fc5b795
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-10 13:03:38 +0700

    Update store.ts

--- src/typescript/redux/store.ts
+++ src/typescript/redux/store.ts
@@ -1,1 +1,6 @@
+import {createStore, applyMiddleware, compose, Store} from 'redux'
+import {saga} from '@redux/saga/saga'
+import {rootReducer} from '@redux/reducers/rootReducer'
 
+
+export const store: Store = createStore(rootReducer, compose(applyMiddleware(saga)));
