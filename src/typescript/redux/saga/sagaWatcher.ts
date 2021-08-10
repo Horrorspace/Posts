a95a96a956bc77761d86d1ac8a93b17dc4520509
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-10 13:02:24 +0700

    Update sagaWatcher.ts

--- src/typescript/redux/saga/sagaWatcher.ts
+++ src/typescript/redux/saga/sagaWatcher.ts
@@ -1,1 +1,8 @@
+import {takeEvery, put} from 'redux-saga/effects'
+import {PostActTypes} from '@redux/types/PostActTypes'
+import {sagaUpdate} from '@redux/saga/sagaUpdate'
 
+
+export function* sagaWatcher() {
+    yield takeEvery(PostActTypes.downloadPosts, sagaUpdate);
+}
