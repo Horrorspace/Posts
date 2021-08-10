39bb022225127b9aaf4e5bc164bc3df870bbe408
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-10 12:56:52 +0700

    Update rootReducer.ts

--- src/typescript/redux/reducers/rootReducer.ts
+++ src/typescript/redux/reducers/rootReducer.ts
@@ -1,1 +1,7 @@
+import {combineReducers, Reducer} from 'redux'
+import {postReducer} from '@redux/reducers/postReducer'
 
+
+export const rootReducer: Reducer = combineReducers({
+    post: postReducer,
+});
