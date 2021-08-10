e6b48f83a4d3a40abe8b423537dbed9c36789a99
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-10 12:55:49 +0700

    Update postReducer.ts

--- src/typescript/redux/reducers/postReducer.ts
+++ src/typescript/redux/reducers/postReducer.ts
@@ -4,19 +4,39 @@ import { IPost, IPostData, IPostInstance, IPosts, INewPost } from '@core/interfa
 import postCheck from '@core/functions/postCheck'
 import Post from '@core/classes/Post'
 import Posts from '@core/classes/Posts'
+import downloadPosts from '@core/functions/downloadPosts'
+import sendNewPost from '@core/functions/sendNewPost'
 
 
 const defaultState: IreduxState = {
     posts: [],
-    isDataLoaded: true,
     isDataUpdating: false
 };
 
-export const calcReducer: Reducer = (state: IreduxState = defaultState, action: IreduxAction): IreduxState => {
+export const postReducer: Reducer = (state: IreduxState = defaultState, action: IreduxAction): IreduxState => {
     switch (action.type) {
         case PostActTypes.downloadPosts:
+            downloadPosts();
             return {
-                ...state
+                ...state,
+                isDataUpdating: true
+            }
+        case PostActTypes.sendNewPost:
+            sendNewPost(action.post);
+            return {
+                ...state,
+                isDataUpdating: true
+            }
+        case PostActTypes.putPost:
+            return {
+                ...state,
+                isDataUpdating: true
+            }
+        case PostActTypes.updatePosts:
+            return {
+                ...state,
+                posts: action.posts,
+                isDataUpdating: false
             }
     }
 }
