69ebe67bcedbdcccc0929809e3ebc7dec5825857
Eugeniy <60815345+Horrorspace@users.noreply.github.com>
2021-08-11 12:57:34 +0700

    Update postActions.ts

--- src/typescript/redux/actions/postActions.ts
+++ src/typescript/redux/actions/postActions.ts
@@ -1,10 +1,12 @@
 import {PostActTypes} from '@redux/types/PostActTypes'
 import {IreduxAction} from '@redux/interfaces/IRedux'
 import { IPostInstance, IPosts, INewPost } from '@core/interfaces/IPost'
+import downloadPosts from '@core/functions/downloadPosts'
+
 
 export const downloadPosts = (): IreduxAction => {
     return async (dispatch) => {
-        const posts = await downloadPosts();
+        const posts: IPosts = await downloadPosts();
         dispatch(updatePosts(posts));
     }
     
@@ -28,5 +30,10 @@ export const updatePosts = (posts: IPosts): IreduxAction => {
     return {
         type: PostActTypes.putPost,
         posts
+    }
+};
+export const updatePosts = (): IreduxAction => {
+    return {
+        type: PostActTypes.setDefault,
     }
 };
