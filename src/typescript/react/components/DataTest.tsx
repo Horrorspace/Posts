e2fa60f7fdbb74a4061a6cb5c67a35b45d923a52
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-08-13 07:59:53 +0700

    react edit

--- src/typescript/react/components/DataTest.tsx
+++ src/typescript/react/components/DataTest.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import {IAction, IThunkAction, IreduxState, IRootReducer} from '@redux/interfaces/IRedux'
 import {Provider, useSelector, useStore} from 'react-redux'
 import { Store } from 'redux'
@@ -7,6 +7,7 @@ import { IPost, IPostData, IPostInstance, IPosts, INewPost } from '@core/interfa
 import Post from '@core/classes/Post'
 import Posts from '@core/classes/Posts'
 import {downloadPosts, setDefault, sendNewPost} from '@redux/actions/postActions'
+import {Modal, Button} from 'react-bootstrap'
 
 
 
@@ -41,19 +42,46 @@ setTimeout(() => {
 
 export const DataTest: React.FC = () => {
     const posts: IPostInstance[] = useSelector((state: IRootReducer) => state.post.posts.getAllPosts());
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     
-    const postList = posts.map(
+    const postTitleList: ReactElement[] = posts.map(
         (post: IPostInstance): ReactElement => {
             return (
-                <li>{post.getTitle()}</li>
+                <li>
+                    <Button variant="primary" onClick={handleShow}>
+                        {post.getTitle()}
+                    </Button>
+                </li>
+            )
+        }
+    );
+    const postList: ReactElement[] = posts.map(
+        (post: IPostInstance): ReactElement => {
+            return (
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{post.getTitle()}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>{post.getBody()}</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                            Edit
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             )
         }
     );
     
     return (
-            <div>
-                {postList}
-            </div>
+            <ul>
+                {postTitleList}
+            </ul>
     )
 }
