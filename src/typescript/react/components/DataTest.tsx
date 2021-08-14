bc0e33e3b9552cd458f52c2c0918a4364aa77723
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-08-14 09:49:57 +0700

    react and scss edit

--- src/typescript/react/components/DataTest.tsx
+++ src/typescript/react/components/DataTest.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useState, MouseEvent } from 'react'
 import {IAction, IThunkAction, IreduxState, IRootReducer} from '@redux/interfaces/IRedux'
 import {Provider, useSelector, useStore} from 'react-redux'
 import { Store } from 'redux'
@@ -7,7 +7,7 @@ import { IPost, IPostData, IPostInstance, IPosts, INewPost } from '@core/interfa
 import Post from '@core/classes/Post'
 import Posts from '@core/classes/Posts'
 import {downloadPosts, setDefault, sendNewPost} from '@redux/actions/postActions'
-import {Modal, Button} from 'react-bootstrap'
+import {Modal, Button, Container} from 'react-bootstrap'
 
 
 
@@ -42,26 +42,33 @@ setTimeout(() => {
 
 export const DataTest: React.FC = () => {
     const posts: IPostInstance[] = useSelector((state: IRootReducer) => state.post.posts.getAllPosts());
+    const idArr: number[] = posts.map((val: IPostInstance) => val.getId());
     const [show, setShow] = useState(false);
+    const [activeId, setActiveId] = useState(0);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = (e: MouseEvent<HTMLButtonElement>) => {
+        const target = e.target as HTMLButtonElement;
+        const id: number = parseInt(target.id, 10);
+        setActiveId(id);
+        setShow(true);
+    };
     
     const postTitleList: ReactElement[] = posts.map(
         (post: IPostInstance): ReactElement => {
             return (
-                <li>
-                    <Button variant="primary" onClick={handleShow}>
+                <Container fluid="sm" className="d-flex flex-column justify-content-center">
+                    <Button variant="primary" onClick={handleShow} id={`${post.getId()}`}>
                         {post.getTitle()}
                     </Button>
-                </li>
+                </Container>
             )
         }
     );
     const postList: ReactElement[] = posts.map(
         (post: IPostInstance): ReactElement => {
             return (
-                <Modal show={show} onHide={handleClose}>
+                <Modal show={show && activeId == post.getId()} onHide={handleClose}>
                     <Modal.Header closeButton>
                         <Modal.Title>{post.getTitle()}</Modal.Title>
                     </Modal.Header>
@@ -80,8 +87,9 @@ export const DataTest: React.FC = () => {
     );
     
     return (
-            <ul>
+            <Container fluid>
                 {postTitleList}
-            </ul>
+                {postList}
+            </Container>
     )
 }
