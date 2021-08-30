4bd13a60a6189673aa4f53d79f0c42dd241fc7b6
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-08-30 08:11:51 +0700

    auth edit

--- src/typescript/core/constants/urlConst.ts
+++ src/typescript/core/constants/urlConst.ts
@@ -1,2 +1,2 @@
-export const url: string = 'http://37.193.148.113/:3007/api/posts/';
-export const authUrl: string = 'http://37.193.148.113/:3007/api/auth/';
+export const url: string = 'http://37.193.148.113:3007/api/posts/';
+export const authUrl: string = 'http://37.193.148.113:3007/api/auth/';
--- src/typescript/core/functions/downloadAllPosts.ts
+++ src/typescript/core/functions/downloadAllPosts.ts
@@ -3,31 +3,33 @@ import postCheck from '@core/functions/postCheck'
 import Post from '@core/classes/Post'
 import Posts from '@core/classes/Posts'
 import { from, Subscription } from 'rxjs'
+import {fromFetch} from 'rxjs/fetch'
 import { ajax, AjaxConfig } from 'rxjs/ajax'
 import { filter, map, switchMap } from 'rxjs/operators'
 import { url } from '@core/constants/urlConst'
 
 
-export default function downloadAllPosts(): Promise<IPosts> {
+export default async function downloadAllPosts(): Promise<IPosts> {
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    const respons: Response = await fetch(url, {
+        headers,
+        mode: 'no-cors'
+    });
+    console.log(respons);
+    const rawData: any = await respons.json();
+    console.log(rawData);
+
+
     return new Promise(resolve => {
-        const headers = {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        };
-        const ajaxOptions: AjaxConfig = {
-            url,
-            headers,
-            withCredentials: true,
-            crossDomain: true
-        }
         const posts: IPosts = new Posts();
-        const data$ = ajax.getJSON(url, headers).pipe(
-            //map((val) => console.log(val)),
-            switchMap((val: any) => from(val)),
+        const data$ = from(rawData).pipe(
             filter((val: any) => typeof(val) === 'object'),
             filter((val: any) => postCheck(val)),
             map((val: IPostData): IPostInstance => new Post(val))
-        );
+        )
         const sub: Subscription = data$.subscribe({
             next: (val: IPostInstance) => {
                 posts.addPost(val);
@@ -37,4 +39,48 @@ export default function downloadAllPosts(): Promise<IPosts> {
             }
         });
     })
+
+    
+    
+    // return new Promise(resolve => {
+    //     const headers = {
+    //         'Content-Type': 'application/json',
+    //         'Origin': 'http://37.193.148.113:3007',
+    //         'Accept': 'application/json'
+    //     };
+    //     const ajaxOptions: AjaxConfig = {
+    //         url,
+    //         headers,
+    //         withCredentials: true,
+    //         crossDomain: true
+    //     }
+    //     const posts: IPosts = new Posts();
+    //     // const data$ = ajax.getJSON(url, headers).pipe(
+    //     //     //map((val) => console.log(val)),
+    //     //     switchMap((val: any) => from(val)),
+    //     //     filter((val: any) => typeof(val) === 'object'),
+    //     //     filter((val: any) => postCheck(val)),
+    //     //     map((val: IPostData): IPostInstance => new Post(val))
+    //     // );
+    //     const data$ = ajax(url).pipe(
+    //         map(val => {
+    //             console.log(val);
+    //             return val.response as string
+    //         }),
+    //         map(val => JSON.parse(val)),
+    //         switchMap((val: any) => from(val)),
+    //         filter((val: any) => typeof(val) === 'object'),
+    //         filter((val: any) => postCheck(val)),
+    //         map((val: IPostData): IPostInstance => new Post(val))
+    //     );
+    //     const sub: Subscription = data$.subscribe({
+    //         next: (val: IPostInstance) => {
+    //             posts.addPost(val);
+    //         },
+    //         complete: () => {
+    //             resolve(posts)
+    //         }
+    //     });
+    // })
+
 }
